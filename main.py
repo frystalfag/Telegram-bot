583da647506d9c1ed9f1b0664af0d991606d2c81
Maksym <makspon0706@gmail.com>
2024-10-17 17:06:44 +0300

    Add xp, game

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import telebot
 import sqlite3
 from telebot import types
-from dir import Dir
+from dir import words
+import random
 
 bot = telebot.TeleBot('7870390365:AAH2U5ElAoactg7XN44zhfgJkykBtC1P1Cs')
 
@@ -48,6 +49,36 @@ def start(message):
     markup.add(button)
 
     bot.send_message(message.chat.id, f'🌍 Мовна подорож: відкривай нові міста та країни, виконуючи завдання на переклад слів, фраз, та граматику.\n', reply_markup=markup)
+
+@bot.message_handler(commands=['play'])
+def start_game(message):
+    english_word, ukrainian_translation = random.choice(list(words.items()))
+    bot.send_message(message.chat.id, f"Переклади це слово: *{english_word}* з англійської на українську!", parse_mode='Markdown')
+    bot.register_next_step_handler(message, check_translation, english_word, ukrainian_translation)
+
+
+def check_translation(message, english_word, ukrainian_translation):
+    user_answer = message.text.strip().lower()
+
+    if user_answer == ukrainian_translation:
+        bot.send_message(message.chat.id, "✅ Правильно! Ти молодець!")
+        add_xp(message.from_user.username, 10)
+    else:
+        bot.send_message(message.chat.id, f"❌ Неправильно. Правильна відповідь: *{ukrainian_translation}*.", parse_mode='Markdown')
+
+def add_xp(username, xp):
+    connect = db_conn()
+    cursor = connect.cursor()
+
+    cursor.execute('SELECT * FROM User WHERE Name=?', (username,))
+    user = cursor.fetchone()
+
+    if user:
+        new_xp = user[2] + xp
+        cursor.execute('UPDATE User SET XP=? WHERE Name=?', (new_xp, username))
+        connect.commit()
+
+    connect.close()
 
 # @bot.message_handler(commands=['Add'])
 bot.polling()
